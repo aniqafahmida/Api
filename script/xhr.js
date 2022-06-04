3f6e0c017034d8f7862ae6d6df4ad0280bc5a99c
Anika Fahmida <anikafahmidahossain29@gmail.com>
2022-06-05 01:14:43 +0600

    init_commit

---
+++ script/xhr.js
@@ -0,0 +1,55 @@
+console.clear();
+  
+
+
+const makeRequest = (method , url ,data ) =>{
+    const xhr = new XMLHttpRequest();
+    xhr.open(method ,url);
+
+     xhr.setRequestHeader('Content-Type' , 'application/json')
+
+
+    xhr.onload = () => {
+        let data= xhr.response;
+        console.log(JSON.parse(data))
+    }
+
+    xhr.onerror = () => {
+        console.log('error is here')
+    }
+
+
+    xhr.send(JSON.stringify(data));
+
+
+
+}
+
+
+const getData = () => {
+    makeRequest('GET' , 'https://jsonplaceholder.typicode.com/posts')
+    
+
+}
+const sendData = () => {
+    makeRequest('POST' , 'https://jsonplaceholder.typicode.com/posts' , {
+        title: 'foo',
+    body: 'bar',
+    userId: 1,
+    })
+    
+
+}
+const updateData = () => {
+    makeRequest('PUT' , 'https://jsonplaceholder.typicode.com/posts/1' , {
+        id: 1,
+        title: 'fooMA',
+        body: 'barMA',
+        userId: 1,
+    })
+    
+
+}
+
+updateData();
+
